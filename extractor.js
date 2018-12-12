59b7848a578ac85c5173229fac59634cea25e281
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2018-12-12 11:04:50 -0600

    Switch RegEx

--- extractor.js
+++ extractor.js
@@ -1,53 +1,73 @@
-const fs = require('fs')
-var iconvlite = require('iconv-lite')
 const chardet = require('chardet')
-const archivoMenuPrincipal = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/Intelisis3100/Codigo Original/MenuPrincipal.dlg'
-const archivoDLGMAVI = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/Intelisis3100/Reportes MAVI/MenuPrincipal_DLG_MAVI.esp'
-var array = ['Mov.Ventas', 'Mov.Inventarios']
-const recodificacion = 'Latin1'
-const nuevaCarpeta= 'ArchivosNEW/'
+const fs = require('fs')
+const iconvlite = require('iconv-lite')
 
-function remplazarTexto (archivo, texto) {
-  fs.writeFile(nuevaCarpeta+archivo.replace(/.*\//, ''), texto, function (err) {
-    if (err) {
-      return console.log(err)
-    }
-    console.log('CODIFICACION ALMACENADA: ' + chardet.detectFileSync(nuevaCarpeta+archivo.replace(/.*\//, '')) + '  --  ' + archivo.replace(/.*\//g, ''))
-  })
+const archivoDLGMAVI = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/Intelisis3100/Reportes MAVI/MenuPrincipal_DLG_MAVI.esp'
+const archivoMenuPrincipal = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/Intelisis3100/Codigo Original/MenuPrincipal.dlg'
+const arreglo = ['Mov.Ventas', 'Mov.Inventarios']
+const nuevaCarpeta= 'ArchivosNEW/'
+const recodificacion = 'Latin1'
+
+function crearExpresion (arreglo, posicion) {
+  return new RegExp(`\\[.*?${transformar('agregarEscape',arreglo[posicion])}[^*]*?(?=\\[)`, `g`)
 }
 
-function extraer (archivo, array, texto) {
-  texto = transformar(1, texto)
-  texto = texto + '\n['
+function extraer (archivo, arreglo, texto) {
+  texto = transformar('borrarTexto', texto + '\n[')
   let extraccionCompleta = ''
-  for(let posicion =0; posicion < array.length; posicion++) {
-    let expresion = crearExpresion (array, posicion)
-    var res = texto.match(expresion)
-    let newres = JSON.stringify(res)
-    let extraccion = newres.match(/Nombre\=.*?(?=\\)|(?<=\\n)Menu\=.*?(?=\\)|NombreDesplegar\=.*?(?=\\)|TipoAccion\=.*?(?=\\)|ClaveAccion\=.*?(?=\\)/gm)
-    let newextraccion = JSON.stringify(extraccion)
-    extraccionCompleta += newextraccion + '\n\n'
-    extraccionCompleta = extraccionCompleta.replace(/[\[\"\]]/g, '').replace(/\,/gm, '\n')
-    console.log(`${posicion} : ${archivo.replace(/.*\//, '')} --- ${array[posicion]} \nRegEx creada:  ${expresion} \nExtraccion: ${newextraccion.replace(/[\[\"\]]/g, '').replace(/\,/gm, ', ')}\n\n`)
+  for(let posicion =0; posicion < arreglo.length; posicion++) {
+    extraccionCompleta = extraerTexto(archivo, crearExpresion(arreglo, posicion), extraccionCompleta, posicion, texto)
   }
   return extraccionCompleta
 }
-function crearExpresion (array, posicion) {
-  let arrayExpresion = array[posicion].replace(/\./g, '\\.')
-  let preExpresion = `\\[.*?${arrayExpresion}[^*]*?(?=\\[)`
-  var expresion = new RegExp(preExpresion, "g")
-  return expresion
-}
 
-function recodificar(archivo, recodificacion) {
-  var texto = fs.readFileSync(archivo)
-  return iconvlite.decode(texto, recodificacion)
+function extraerTexto (archivo, expresion, extraccionCompleta, posicion, texto) {
+  let extraccionGeneral = transformar('matchGeneral', JSON.stringify(texto.match(expresion)))
+  let extraccionReducida = JSON.stringify(extraccionGeneral)
+  extraccionCompleta += extraccionReducida + '\n\n'
+  extraccionCompleta = transformar('limpiarCadena', extraccionCompleta)
+  console.log(`${posicion} : ${transformar('reducirRuta', archivo)} --- ${arreglo[posicion]} \n`+
+              `RegEx creada:  ${expresion} \nExtraccion: \n${transformar('limpiarCadena',extraccionReducida)}\n`)
+  return extraccionCompleta
 }
 
 function procesar (archivo, recodificacion) {
-  let textoRecodificado = recodificar(archivo, recodificacion)
-  let extraccion = extraer(archivo, array, textoRecodificado)
-  remplazarTexto(archivo, extraccion)
+  remplazarTexto(archivo, extraer(archivo, arreglo, recodificar(archivo, recodificacion)))
+}
+
+function recodificar(archivo, recodificacion) {
+  return iconvlite.decode(fs.readFileSync(archivo), recodificacion)
+}
+
+function remplazarTexto (archivo, texto) {
+  fs.writeFile(nuevaCarpeta + transformar('reducirRuta', archivo), texto, function (error) {
+    if (error) {
+      return console.log(error)
+    }
+    console.log('CODIFICACION ALMACENADA: ' +
+                 chardet.detectFileSync(nuevaCarpeta+transformar('reducirRuta', archivo)) +
+                 '  --  ' + transformar('reducirRuta', archivo))
+  })
+}
+
+function transformar (opcion, texto) {
+  switch(opcion){
+    case 'borrarTexto': {
+      return texto.replace(/\;.*/g, '').replace(/\&/g, '')
+    }
+    case 'reducirRuta': {
+      return texto.replace(/.*\//, '')
+    }
+    case 'agregarEscape': {
+      return texto.replace(/\./g, '\\.')
+    }
+    case 'limpiarCadena': {
+      return texto = texto.replace(/[\[\"\]]/g, '').replace(/\,/gm, '\n')
+    }
+    case 'matchGeneral': {
+      return texto.match(/Nombre\=.*?(?=\\)|(?<=\\n)Menu\=.*?(?=\\)|NombreDesplegar\=.*?(?=\\)|TipoAccion\=.*?(?=\\)|ClaveAccion\=.*?(?=\\)/gm)
+    }
+  }
 }
 
 procesar(archivoMenuPrincipal, recodificacion)
