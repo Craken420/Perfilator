d9df776df7d6652e219bf340083e26bc7d0d0ca9
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2018-12-12 11:01:30 -0600

    Extraer con expresiones

--- extractor.js
+++ extractor.js
@@ -1,22 +1,51 @@
 const fs = require('fs')
 var iconvlite = require('iconv-lite')
 const chardet = require('chardet')
-const archivo = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/Intelisis3100/Codigo Original/MenuPrincipal.dlg'
+const archivoMenuPrincipal = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/Intelisis3100/Codigo Original/MenuPrincipal.dlg'
+const archivoDLGMAVI = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/Intelisis3100/Reportes MAVI/MenuPrincipal_DLG_MAVI.esp'
+var array = ['Mov.Ventas', 'Mov.Inventarios']
 const recodificacion = 'Latin1'
 const nuevaCarpeta= 'ArchivosNEW/'
 
 function remplazarTexto (archivo, texto) {
-  fs.writeFile(nuevaCarpeta+archivo.replace(/.*\//g, ''), texto, function (err) {
+  fs.writeFile(nuevaCarpeta+archivo.replace(/.*\//, ''), texto, function (err) {
     if (err) {
       return console.log(err)
     }
-    console.log('CODIFICACION ALMACENADA: ' + chardet.detectFileSync(nuevaCarpeta+archivo.replace(/.*\//g, '')) + '  --  ' + archivo.replace(/.*\//g, ''))
+    console.log('CODIFICACION ALMACENADA: ' + chardet.detectFileSync(nuevaCarpeta+archivo.replace(/.*\//, '')) + '  --  ' + archivo.replace(/.*\//g, ''))
   })
 }
 
-function transformar (texto) {
-  texto = texto.replace(/\;.*/g, '')
-  return texto
+function extraer (archivo, array, texto) {
+  texto = transformar(1, texto)
+  texto = texto + '\n['
+  let extraccionCompleta= ''
+  for(let i =0; i<array.length; i++){
+    let arrayExpresion = array[i].replace(/\./g, '\\.')
+    let preExpresion = `\\[.*?${arrayExpresion}[^*]*?(?=\\[)`
+    var expresion = new RegExp(preExpresion, "g")
+    var res = texto.match(expresion)
+    let newres = JSON.stringify(res)
+    let extraccion = newres.match(/Nombre\=.*?(?=\\)|(?<=\\n)Menu\=.*?(?=\\)|NombreDesplegar\=.*?(?=\\)|TipoAccion\=.*?(?=\\)|ClaveAccion\=.*?(?=\\)/gm)
+    let newextraccion = JSON.stringify(extraccion)
+    extraccionCompleta += newextraccion + '\n\n'
+    extraccionCompleta = extraccionCompleta.replace(/[\[\"\]]/g, '').replace(/\,/gm, '\n')
+    console.log(`${i} : ${archivo.replace(/.*\//, '')} --- ${array[i]} \nRegEx creada:  ${expresion} \nExtraccion: ${newextraccion.replace(/[\[\"\]]/g, '').replace(/\,/gm, ', ')}\n\n`)
+  }
+  return extraccionCompleta
+}
+
+function transformar (opcion, texto) {
+  switch(opcion){
+    case 1: {
+      texto = texto.replace(/\;.*/g, '')
+      texto = texto.replace(/\&/g, '')
+      return texto
+    }
+  }
+  // texto = texto.replace(/\;.*/g, '')
+  // texto = texto.replace(/\&/g, '')
+  // return texto
 }
 
 function recodificar(archivo, recodificacion) {
@@ -26,7 +55,9 @@ function recodificar(archivo, recodificacion) {
 
 function procesar (archivo, recodificacion) {
   let textoRecodificado = recodificar(archivo, recodificacion)
-  remplazarTexto(archivo, transformar(textoRecodificado))
+  let extraccion = extraer(archivo, array, textoRecodificado)
+  remplazarTexto(archivo, extraccion)
 }
 
-procesar(archivo, recodificacion)
+procesar(archivoMenuPrincipal, recodificacion)
+procesar(archivoDLGMAVI, recodificacion)
