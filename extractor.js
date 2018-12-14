8b247deabab681c409a733b20fa0d32b8e6a6090
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2018-12-14 11:00:07 -0600

    JSON Metodos

--- extractor.js
+++ extractor.js
@@ -1,4 +1,3 @@
-const chardet = require('chardet')
 const fs = require('fs')
 const iconvlite = require('iconv-lite')
 
@@ -11,46 +10,44 @@ exports.archivoMenuPrincipal = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/I
 exports.recodificacion = 'Latin1'
 
 const jsonRegEx = {
-  'clsTexto' : {
-                'clsComentarios': /\;.*/g,
-                'clsAmpersand':   /\&/g,
-                'clsNull':        /null/gm,
-               },
-  'reducirRuta': /.*\//,
-  'buscarPunto': /\./g,
-  'limpiarCadena': {
-                     'limpiarObjeto': /[\\\[\"\]]/g,
-                     'buscarComa':    /\,/gm
-                   },
-  'buscarCampos': {
-    'todos':/Nombre\=.*?(?=\\)|(?<=\\n)Menu\=.*?(?=\\)|NombreDesplegar\=.*?(?=\\)|TipoAccion\=.*?(?=\\)|ClaveAccion\=.*?(?=\\)/gm,
-    'nombre':/(?<=Nombre\=).*/gi,
-    'menu':/(?<=Menu\=).*/gm,
-    'nombreDesplegar':/(?<=NombreDesplegar\=).*/gm,
-    'tipoAccion':/(?<=TipoAccion\=).*/gm,
-    'claveAccion':/(?<=ClaveAccion\=).*/gm,
+  'clsComentarios': /\;.*/g,
+  'clsAmpersand':   /\&/g,
+  'clsNull':        /null/gm,
+  'reducirRuta':    /.*\//,
+  'buscarPunto':    /\./g,
+  'limpiarObjeto':  /[\\\[\"\]]/g,
+  'buscarComa':     /\,/gm,
+  'todos': /Nombre\=.*?(?=\\)|(?<=\\n)Menu\=.*?(?=\\)|NombreDesplegar\=.*?(?=\\)|TipoAccion\=.*?(?=\\)|ClaveAccion\=.*?(?=\\)/gm,
+  'nombre': /(?<=Nombre\=).*/gi,
+  'menu': /(?<=Menu\=).*/gm,
+  'nombreDesplegar': /(?<=NombreDesplegar\=).*/gm,
+  'tipoAccion': /(?<=TipoAccion\=).*/gm,
+  'claveAccion': /(?<=ClaveAccion\=).*/gm,
+  'metodos': {
+    'limpiarTexto':   (texto) => {return texto.replace(jsonRegEx.clsComentarios, '').replace(jsonRegEx.clsAmpersand, '')},
+    'limpiarObjeto':  (texto) => {return texto.replace(jsonRegEx.limpiarObjeto, '').replace(jsonRegEx.buscarComa, '\n')},
+    'remplazarPunto': (arreglo, posicion) => {return arreglo[posicion].replace(jsonRegEx.buscarPunto, '\\.')}
   }
 }
 
 function crearExpresion (arreglo, posicion) {
-  return new RegExp(`\\[.*?${arreglo[posicion].replace(jsonRegEx.buscarPunto, '\\.')}[^*]*?(?=\\[)`, `g`)
+  return new RegExp(`\\[.*?${jsonRegEx.metodos.remplazarPunto(arreglo, posicion)}[^*]*?(?=\\[)`, `g`)
 }
 
-function extraer (archivo, arreglo, texto) {
-  texto = texto.replace(jsonRegEx.clsTexto.clsComentarios, '').replace(jsonRegEx.clsTexto.clsAmpersand, '') + '\n['
-  var objetoPerfiles = {}
+function extraer (arreglo, texto) {
+  texto = texto + '\n['
+  texto = jsonRegEx.metodos.limpiarTexto(texto)
+  
+  let objetoPerfiles = {}
   let extraccionReducida = ''
-  for(let posicion =0; posicion < arreglo.length; posicion++) {
 
-    let expresion = crearExpresion(arreglo, posicion)
-    extraccionReducida = extraerTexto(expresion,texto)
+  for(let posicion = 0; posicion < arreglo.length; posicion++) {
 
-    if (extraccionReducida == 'null') {
-      //console.log('extractor no encontro coincidencia')
-    } else {
+    extraccionReducida = extraerTexto(crearExpresion(arreglo, posicion), texto)
+
+    if (extraccionReducida !== 'null') {
 
       let datosObjeto = extraerTextoObjeto(extraccionReducida)
-      extraerTextoObjeto(extraccionReducida, arreglo, posicion)
 
       objetoPerfiles[arreglo[posicion]] = {
         'nombre': datosObjeto.nombreMenuItem,
@@ -65,10 +62,7 @@ function extraer (archivo, arreglo, texto) {
 }
 
 function extraerTexto(expresion, texto) {
-  let extraccion = JSON.stringify(texto.match(expresion)).match(jsonRegEx.buscarCampos.todos)
-    let extraccionString = JSON.stringify(extraccion)
-    extraccionString = extraccionString.replace(jsonRegEx.limpiarCadena.limpiarObjeto, '').replace(jsonRegEx.limpiarCadena.buscarComa, '\n')
-    return extraccionString
+  return jsonRegEx.metodos.limpiarObjeto(JSON.stringify( JSON.stringify(texto.match(expresion)).match(jsonRegEx.todos)))
 }
 
 function extraerTextoObjeto(extraccionReducida) {
@@ -78,20 +72,20 @@ function extraerTextoObjeto(extraccionReducida) {
   let tipoAccionItem = ''
   let claveAccionItem = ''
 
-  nombreMenuItem = extraccionReducida.match(jsonRegEx.buscarCampos.nombre)
-  menuItem = extraccionReducida.match(jsonRegEx.buscarCampos.menu)
-  nombreDesplegarItem = extraccionReducida.match(jsonRegEx.buscarCampos.nombreDesplegar)
-  tipoAccionItem = extraccionReducida.match(jsonRegEx.buscarCampos.tipoAccion)
-  claveAccionItem = extraccionReducida.match(jsonRegEx.buscarCampos.claveAccion)
+  nombreMenuItem = extraccionReducida.match(jsonRegEx.nombre)
+  menuItem = extraccionReducida.match(jsonRegEx.menu)
+  nombreDesplegarItem = extraccionReducida.match(jsonRegEx.nombreDesplegar)
+  tipoAccionItem = extraccionReducida.match(jsonRegEx.tipoAccion)
+  claveAccionItem = extraccionReducida.match(jsonRegEx.claveAccion)
 
-  nombreMenuItem = JSON.stringify(nombreMenuItem).replace(jsonRegEx.limpiarCadena.limpiarObjeto, '')
-  menuItem = JSON.stringify(menuItem).replace(jsonRegEx.limpiarCadena.limpiarObjeto, '') 
-  nombreDesplegarItem = JSON.stringify(nombreDesplegarItem).replace(jsonRegEx.limpiarCadena.limpiarObjeto, '')
-  tipoAccionItem = JSON.stringify(tipoAccionItem).replace(jsonRegEx.limpiarCadena.limpiarObjeto, '')
-  claveAccionItem = JSON.stringify(claveAccionItem).replace(jsonRegEx.limpiarCadena.limpiarObjeto, '')
+  nombreMenuItem = jsonRegEx.metodos.limpiarObjeto(JSON.stringify(nombreMenuItem))
+  menuItem = jsonRegEx.metodos.limpiarObjeto(JSON.stringify(menuItem)) 
+  nombreDesplegarItem = jsonRegEx.metodos.limpiarObjeto(JSON.stringify(nombreDesplegarItem))
+  tipoAccionItem = jsonRegEx.metodos.limpiarObjeto(JSON.stringify(tipoAccionItem))
+  claveAccionItem = jsonRegEx.metodos.limpiarObjeto(JSON.stringify(claveAccionItem))
   
   return {
-    nombreMenuItem :nombreMenuItem,
+    nombreMenuItem: nombreMenuItem,
     menuItem: menuItem,
     nombreDesplegarItem: nombreDesplegarItem,
     tipoAccionItem: tipoAccionItem,
@@ -99,12 +93,10 @@ function extraerTextoObjeto(extraccionReducida) {
   }
 }
 
-exports.procesarArreglo = function(archivo, recodificacion, arreglo) {
-  var extraccion = extraer(archivo, arreglo, recodificar(archivo, recodificacion))
-  return extraccion
+exports.procesarArreglo = (archivo, recodificacion, arreglo) => {
+  return extraer(arreglo, recodificar(archivo, recodificacion))
 }
 
 function recodificar(archivo, recodificacion) {
-  let extraccionMenuP = iconvlite.decode(fs.readFileSync(archivo), recodificacion)
-  return extraccionMenuP
+  return iconvlite.decode(fs.readFileSync(archivo), recodificacion)
 }
--- servidor.js
+++ servidor.js
@@ -53,56 +53,23 @@ aplicacion.get('/Menus/:Usuario', (solicitud, respuesta) => {
         let item = respuestaSQL[i]["item"]
         consumo.push(item)
       }
+
       let extraccionMenuP = extractor.procesarArreglo(extractor.archivoMenuPrincipal, extractor.recodificacion, consumo)
       let extraccionDLGMAVI= extractor.procesarArreglo(extractor.archivoDLGMAVI, extractor.recodificacion, consumo)
-      // console.log(extraccionMenuP)
-      // console.log(extraccionDLGMAVI)
-      console.log('for')
+    
       for (key in extraccionMenuP) {
-        let cadena = [key]
-        console.log('\n-----------------------------------------\n')
-        console.log(cadena)
-      
+
         if (extraccionDLGMAVI[key]!=undefined) {
-            console.log('\ncoincidencia con : '+ [key] +'  en el extraccionDLGMAVI\n')
-            console.log('\n**************************')
-            console.log(`Contenido extraccionDLGMAVI en ${key}`)
-            console.log(extraccionDLGMAVI[key])
-            console.log('\n**************************')
-            console.log('\nObject.getOwnPropertyNames(extraccionDLGMAVI[key]):\n')
-            let cadena = Object.getOwnPropertyNames(extraccionDLGMAVI[key])
-            console.log(cadena)
-            console.log('\n**************************')
-            console.log('\ncadena[key]\n')
-            for (key2 in cadena) {
-              console.log(cadena[key2])
-              console.log('\ncextraccionDLGMAVI[key][cadena]-- valor del campo ejem(extraccionDLGMAVI[EXPAgente][nombreDesplegar]= ¡¡  soy el campo a modificar !!\n')
-              console.log(extraccionDLGMAVI[key][cadena[key2]])
-              console.log('\n**************************')
-              console.log('\nObjeto Sin Cambio\n')
-              console.log(extraccionMenuP[key])
-              console.log('\n**************************')
-              console.log('\nObjeto Con Cambio\n')
-              extraccionMenuP[key][cadena[key2]] = extraccionDLGMAVI[key][cadena[key2]]
-              console.log(extraccionMenuP[key])
-              console.log('\n**************************')
+            let propiedadObj = Object.getOwnPropertyNames(extraccionDLGMAVI[key])
+            for (key2 in propiedadObj) {
+              extraccionMenuP[key][propiedadObj[key2]] = extraccionDLGMAVI[key][propiedadObj[key2]]
             }
         }
-        console.log('\n-----------------------------------------\n')
-        console.log('objetos a eliminar')
-        console.log(extraccionDLGMAVI[key])
-        delete extraccionDLGMAVI[key];
-        console.log('eliminado')
-        console.log('\n-----------------------------------------\n')
+        delete extraccionDLGMAVI[key]
       }
 
-      console.log('\nAl terminal el proceso del for extraccionMenuP')
-      console.log(extraccionMenuP)
-      console.log('\nAl terminal el proceso del for extraccionDLGMAVI')
-      console.log(extraccionDLGMAVI)
-      var objMenuPCambio = Object.assign(extraccionMenuP, extraccionDLGMAVI);
-      console.log('\nAl terminal el proceso del for obj')
-      console.log(objMenuPCambio)
+      var objMenuPCambio = Object.assign(extraccionMenuP, extraccionDLGMAVI)
+      
       respuesta.send(objMenuPCambio)
       sql.close()
     })
